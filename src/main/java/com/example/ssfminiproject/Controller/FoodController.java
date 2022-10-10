c7de4d2661a763e41870475ffe3c9b661abb6ceb
ngshux <107012996+ngshux@users.noreply.github.com>
2022-10-10 11:38:34 +0800

    login

--- src/main/java/com/example/ssfminiproject/Controller/FoodController.java
+++ src/main/java/com/example/ssfminiproject/Controller/FoodController.java
@@ -30,7 +30,8 @@ public class FoodController {
     public String indexPage(Model model){
         FoodService foodService = new FoodService();
         model.addAttribute("meals", foodService.getMeals());
-        service.addRecipeToFavourite(user, foodService.getMeals().get(0));
+        model.addAttribute("user", user);
+        System.out.println(user);
         return "index";
     }
 
@@ -42,42 +43,61 @@ public class FoodController {
         return "login";
     }
 
-    @PostMapping (path="/login")
-    public String SubmitLogin(@RequestParam String name, Model model) {
-        user.setName(name);
-        service.addUser(user);
+    @PostMapping (path="/submitLogin")
+    public String SubmitLogin(@ModelAttribute User user1, Model model) {
+        model.addAttribute("user", user1);
+        user.setName(user1.getName());
         user.setLoggedIn(true);
-        model.addAttribute("user",user);
-        return "favourites";
+        return "redirect:/favourites";
     }
 
     @PostMapping (path="/logout")
     public String logout(Model model) {
         user.setLoggedIn(false);
-        return "favourites";
+        return "redirect:/";
     }
 
     @PostMapping (path="/addToFav")
-    public String addToFav(@ModelAttribute("user") User user, @RequestParam String strMeal,
+    public String addToFav(@RequestParam String strMeal,
     @RequestParam String strMealThumb, @RequestParam String idMeal) {
-        Meal meal = new Meal(strMeal, strMealThumb, idMeal);
-        System.out.println("ADDING TO FAVOURITES" + meal);
-        service.addRecipeToFavourite(user, meal);
-        return "redirect:/favourites";
+        String recipeLink = "/recipe/"+idMeal;
+        String redirect = user.isLoggedIn() == true ? "redirect:"+recipeLink : "redirect:/login";
+        if(user.isLoggedIn()) {
+            Meal meal = new Meal(strMeal, strMealThumb, idMeal);
+            System.out.println("ADDING TO FAVOURITES" + meal);
+            service.addRecipeToFavourite(user, meal);
+        }
+        return redirect;
     }
 
     @GetMapping("/favourites")
     public String favPage(Model model){
-        System.out.println(service.getFavourites());
-        model.addAttribute("favList", service.getFavourites());
-        return "favourites";
+        System.out.println(service.getFavourites(user));
+        model.addAttribute("user", user);
+        if(user.isLoggedIn()) {
+            model.addAttribute("favList", service.getFavourites(user));
+            return "favourites";
+        }
+        return "login";
+    }
+
+    @PostMapping (path="/removeFavourite")
+    public String removeFromFav(@RequestParam String strMeal,
+                                @RequestParam String strMealThumb, @RequestParam String idMeal) {
+        System.out.println(idMeal);
+        Meal meal = new Meal(strMeal, strMealThumb, idMeal);
+
+        service.removeFromFavourite(user, meal);
+        return "redirect:/favourites";
     }
 
     @GetMapping(path="/recipe/{recipeId}")
     public String recipePage(Model model, @PathVariable String recipeId){
         FoodService foodService = new FoodService();
         Recipe recipe = foodService.findById(recipeId);
+        System.out.println(recipe);
         model.addAttribute("recipe", recipe);
+        model.addAttribute("user", user);
         return "recipe";
     }
 
